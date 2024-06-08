15de8fa4cc55aa1851d814895fde266be10532e8
CatalinPToader <petru.toader0807@stud.acs.upb.ro>
2024-06-08 21:02:56 +0300

    Dockerfile for network, kill script

--- containers/containers_network.sh
+++ containers/containers_network.sh
@@ -3,9 +3,17 @@ read -p "Image to run (e.g debian:latest, ubuntu:18.04): " CONTAINER
 read -p "Name of first container: " C1
 read -p "Name of second container: " C2
 
-sudo docker network create $NETWORK
-ID1=`sudo docker run -d --net $NETWORK --name $C1 $CONTAINER sleep 10000`
-ID2=`sudo docker run -d --net $NETWORK --name $C2 $CONTAINER sleep 10000`
+echo "
+FROM $CONTAINER
 
-echo "ID1=$ID1"
-echo "ID2=$ID2"
+RUN apt update
+RUN apt install iputils-ping -y
+" > network_dockerfile
+
+sudo docker build -f network_dockerfile -t network-con .
+NET=sudo docker network create $NETWORK
+ID1=`sudo docker run -d --net $NETWORK --name $C1 network-con sleep 10000`
+ID2=`sudo docker run -d --net $NETWORK --name $C2 network-con sleep 10000`
+
+echo "ID1: $ID1"
+echo "ID2: $ID2"
